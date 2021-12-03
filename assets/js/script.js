9c73d670bc4f98dab009be97b54c86161fd0f25f
Robert C <robert.clarady@gmail.com>
2021-12-03 09:18:50 -0600

    updated page layout, added search and city framework

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@
 var currentDay= moment().format('dddd, MMMM DD, YYYY');
 $("#currentDay").text(currentDay);
 
+
 var getCityResponse = function(language) {
-  
-  var apiUrl = "https://api.openweather.com";
+  var citySelect ="";
+
+  var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + citySelect + "&APPID=ca0f1bc12f401f207d2783ba0f9c8ba8";
   
   fetch(apiUrl).then(function(response){
       if (response.ok) {
